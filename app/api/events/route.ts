95c421cd70a5436027c6c56be62474f2bafbab6b
Alok Prasad <143709815+alokprasad573@users.noreply.github.com>
2025-10-31 21:42:46 +0530

    Cloudinary Setup Completed.

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,6 +1,8 @@
 import {NextRequest, NextResponse} from "next/server";
 import connectDB from "@/lib/mongodb";
 import Event from "@/database/event.model";
+import {Buffer} from "node:buffer";
+import {v2 as cloudinary} from "cloudinary";
 
 export async function POST(req: NextRequest, res: NextResponse) {
     try {
@@ -16,7 +18,27 @@ export async function POST(req: NextRequest, res: NextResponse) {
            return NextResponse.json({ message: 'Invalid JSON data format'}, { status: 400 });
        }
 
+       const file = formData.get('image') as File;
+       if (!file) {
+           return NextResponse.json({message: 'Image File Required.'}, {status : 400})
+       }
+
+       const arrayBuffer = await file.arrayBuffer();
+       const buffer  =  Buffer.from(arrayBuffer);
+
+       const uploadResult = await new Promise((resolve, reject) => {
+           cloudinary.uploader.upload_stream({ resource_type: 'image', folder: 'DevOrbit'}, (err, results) => {
+               if (err) {
+                   return reject(err);
+               }
+               resolve(results);
+           }).end(buffer);
+       });
+
+       event.image = (uploadResult as {secure_url: string}).secure_url;
+
        const createdEvent: Event = await Event.create(event);
+
        return NextResponse.json({ message: 'Event created successfully', event: createdEvent }, {status: 201});
     } catch (e) {
         return NextResponse.json({
@@ -26,3 +48,14 @@ export async function POST(req: NextRequest, res: NextResponse) {
     }
 }
 
+export async function GET () {
+    try {
+        await connectDB();
+
+        const events = await Event.find().sort({ createdAt: -1 });
+
+        return NextResponse.json({ message: 'Event fetched successfully', events }, { status: 200 });
+    } catch (e) {
+        return NextResponse.json({ message: 'Event fetching failed.', error: e }, { status: 500 });
+    }
+}
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com'
+            }
+        ]
+    },
 
   // Rewrites to support PostHog ingestion endpoints
   async rewrites() {
